728455e0938109e54128b9572c6abd6af9c7d513
TinHtunOo <tinhtunhe@gmail.com>
2024-09-26 17:01:32 +0630

    finish code generator

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-let apiQuotes = []
+let newQuote = document.querySelector(".quote-text");
+let author = document.querySelector(".author");
+let newQuoteBtn = document.querySelector(".new-quote");
+let twitterBtn = document.querySelector(".twitter");
+let quoteBox = document.querySelector(".quote-box");
+let loader = document.querySelector(".loader");
+let quotes = [];
 
-// Get a random quote
-function getRamQuote() {
-    let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log (quote)
+function loading() {
+    loader.hidden = false;
+    quoteBox.hidden = true;
 }
 
-// Get Quote From API
-async function getQuote() {
+function complete() {
+    if(!loader.hidden) {
+        loader.hidden = true;
+        quoteBox.hidden = false;
+    }
+}
+
+// Get Single Quote 
+function getAQuote () {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    if(!quote.author) {
+        author.textContent = "Unknown";
+    } else {
+        author.textContent = quote.author;
+    }
+
+    if(quote.length > 100){
+        newQuote.classList.add("quote-long");
+    } else {
+        newQuote.classList.remove("quote-long");
+    }
+    newQuote.textContent = quote.text;
+    
+}
+
+// Get quotes 
+async function getQuote () {
+    loading()
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        getRamQuote()
+        const apiQuotes = await fetch(apiUrl);
+        quotes = await apiQuotes.json();
+        getAQuote()
     } catch (error) {
         
     }
-
+    complete()
 }
 
-getQuote()
+function tweet(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${newQuote.textContent} - ${author.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener("click", getQuote)
+twitterBtn.addEventListener("click", tweet)
+
+getQuote();
